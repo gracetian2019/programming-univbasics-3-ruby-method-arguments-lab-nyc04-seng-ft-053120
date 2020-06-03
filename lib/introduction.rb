ccf92f362454cf4b8d89c7cb1cf54d47ce22eeea
Grace Tian <gracetian4ever@gmail.com>
2020-06-03 13:52:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language (name, language)
 end
 
 def introduction_with_language_optional(name,language = "Ruby")
-  puts "Hi, my name is #{"
+  puts "Hi, my name is #{name}"
 end
 
 
